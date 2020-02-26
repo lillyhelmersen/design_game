2e271aa5ae4049675e15c34d58a9a61f3bffe96a
lillyhelmersen <lillyhelmersen@users.noreply.github.com>
2020-02-26 12:23:57 +0100

    Added draw for tile

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 /*Downloaded from https://www.codeseek.co/Jastor11/p5js-character-moving-on-screen-dpEQqJ */
-
+//30/50
 //Game
 var posibelItems = [];//1 of alle the posible items
+var tilesize = 50;
+var viewSize = 6;
 //Game bord
-var iland = [];//contains tiles
+var island = [];//contains tiles
 var itemOnbord = []; //all the items that are placed in the world
 
 //Cordinat in array
@@ -31,7 +33,7 @@ var tile = {
   id: 0,
   tileType: "NO",
   image: "NO",
-  cordnat: point,
+  coordinate: point,
   placeCanvas: place,
   //tileItem: item,
   //eddg: false,
@@ -41,6 +43,8 @@ var tile = {
 var player = {
   name: "NO",
   image: "NO",
+  coordinate: point,
+  placeCanvas: place,
   inventory: [],//List of items payer has
 };
 
@@ -53,6 +57,10 @@ var targetY;
 var speed = 50;
 var easing = 0.05;
 
+function prelode() {
+
+}
+
 function setup() {
   createCanvas(400, 400);
   background(0);
@@ -64,6 +72,45 @@ function draw() {
 
 }
 
+function drawView(){
+  var water;//A water tile 0,0
+  var playerCoo = player.coordinate;
+  var drawXat = 0;
+  var drawYat = 0;//tilesize
+
+  for(i = 0; i < playerCoo.y+viewSize; i++){
+    for (j = 0; j < playerCoo.x+viewSize; i++){
+      if(island[i][j] == null){
+        drawTile(0,drawXat,drawYat);
+      } else if (island[i][j].id == 1){
+        drawTile(1,drawXat,drawYat);
+      } else if (island[i][j].id == 0){
+        drawTile(0,drawXat,drawYat);
+      }
+      drawXat += tilesize;
+    }
+    drawYat += tilesize;
+  }
+  /*  player at 7,7
+  draw x-6 to x+6
+  draw y-6 to y+6
+  iff null draw water
+  */
+}
+function drawTile(id, x, y){
+  switch(id){
+    case 0:
+    fill(0);
+    break;
+  case 1:
+    fill(200);
+    break;
+  default:
+    // code block
+    console.log("There was aonce a blog but no more in draw tile");
+  }
+  square(x, y, tilesize);
+}
 //ELIPSES
 function drawElipse(){
   stroke(0);
