e84dfff9b49ac11a1da638f900b6d37ed103b016
Bea <beatrice.trinidad@gmail.com>
2020-02-26 13:17:17 +0100

    Added item variables

---
+++ items.js
@@ -0,0 +1,63 @@
+// Log
+var log = {
+    name: "log",
+    itemType: "NO",
+    image: "img/log.svg",
+    itemPlace: place,
+  };
+
+// Firewood
+var firewood = {
+    name: "firewood",
+    itemType: "NO",
+    image:"img/firewood.svg",
+    itemPlace: place,
+  };
+
+// Flint
+var flint = {
+    name: "flint",
+    itemType: "NO",
+    image:"img/flint.svg",
+    itemPlace: place,
+  };
+
+// Stone
+var stone = {
+    name: "stone",
+    itemType: "NO",
+    image:"img/stone.svg",
+    itemPlace: place,
+  };
+
+// Apple
+var apple = {
+    name: "apple",
+    itemType: "NO",
+    image:"img/apple.svg",
+    itemPlace: place,
+  };
+
+// Brick
+var brick = {
+    name: "brick",
+    itemType: "NO",
+    image:"img/brick.svg",
+    itemPlace: place,
+  };
+
+// Wheat
+var wheat = {
+    name: "wheat",
+    itemType: "NO",
+    image:"img/wheat.svg",
+    itemPlace: place,
+  };
+
+// Diamond
+var wheat = {
+    name: "wheat",
+    itemType: "NO",
+    image:"img/wheat.svg",
+    itemPlace: place,
+  };
